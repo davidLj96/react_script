8d0ecf5de0f2b071553694b325f4eede4a789b39
@dl222qb <dl222qb@student.lnu.se>
2023-05-08 11:32:59 +0200

    second

---
+++ .github/scripts/update_pull_request.py
@@ -0,0 +1,31 @@
+import os
+import re
+from github import Github
+from scan_files import get_usage_info
+
+# dependencies = ['express', 'react', 'fs']
+# repo_name = 'davidLj96/degree_project'
+# commit_sha = 'aassssdafko13fko293jf'
+access_token = os.environ['GITHUB_TOKEN']
+repo_name = os.environ['REPOSITORY']
+pr_number = int(os.environ['PULL_REQUEST'])
+
+g = Github(access_token)
+repo = g.get_repo(repo_name)
+pull_request = repo.get_pull(pr_number)
+pull_request_user = pull_request.user.login
+commit_sha = repo.get_branch(pull_request.head.ref).commit.sha
+
+# Get the commit associated with the pull request
+commit = pull_request.get_commits()[0]
+
+# Get the dependencies from the commit message
+dependency_pattern = r"- dependency-name: (\w+)"
+dependencies = re.findall(dependency_pattern, commit.commit.message)
+
+description = f'## Dependabot\n{pull_request.body}\n\n## Usabot\n'
+description += get_usage_info(dependencies, repo_name, commit_sha)
+
+pull_request.edit(
+    body = description
+)
